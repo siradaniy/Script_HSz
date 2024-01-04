dd0dc5359be5af776c27756a28b28cf0cde0eb2b
siradaniy <83637146+siradaniy@users.noreply.github.com>
2024-01-05 00:47:38 +0700

    Update HSz_Main.lua

--- HSz_Main.lua
+++ HSz_Main.lua
@@ -1,11 +1,11 @@
 local ScriptLoad = nil
-local PlaceId = {}
+local PlaceId_ADS = {6990129309,6990131029,6938803436,7274690025,7338881230,6990133340}
 if game.PlaceId == 8304191830 or game.PlaceId == 8349889591 then
     ScriptLoad = "HSz_AA"
 elseif game.PlaceId == 14433762945 then
     ScriptLoad = "HSz_ACS"
-elseif game.PlaceId == 6990129309 or game.PlaceId == 6938803436 or game.PlaceId == 7274690025 or game.PlaceId == 7338881230 then
-    ScriptLoad = "HSz_ADS"
+elseif table.find(PlaceId_ADS,game.PlaceId) then
+	GameLoad = "HSz_ADS"
 end
 local hsz,err = pcall(function ()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/siradaniy/Script_HSz/main/" .. ScriptLoad .. ".lua"))()
