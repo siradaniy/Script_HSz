05d868e29d1065905bfd4e9eba07b40fc4beb451
siradaniy <83637146+siradaniy@users.noreply.github.com>
2024-02-16 19:22:32 +0700

    Update HSz_Main.lua

--- HSz_Main.lua
+++ HSz_Main.lua
@@ -1,20 +1,20 @@
-local ScriptLoad = nil
+local ScriptLoadHSz = nil
 local PlaceId_ACS = {14433762945,14551143850}
 local PlaceId_AA = {8304191830,8349889591,10098525303,14229762361,14229839966,14918509670}
 local PlaceId_ADS = {6938803436,7274690025,7338881230,6990133340,6990129309,6990131029}
-local PlaceId_ALS = {12900046592,14368918515}
+local PlaceId_ALS = {12900046592,14368918515,12886143095}
 
 if table.find(PlaceId_ACS,game.PlaceId) then
-    ScriptLoad = "HSz_ACS"
+    ScriptLoadHSz = "HSz_ACS"
 elseif table.find(PlaceId_AA,game.PlaceId) then
-    ScriptLoad = "HSz_AA"
+    ScriptLoadHSz = "HSz_AA"
 elseif table.find(PlaceId_ADS,game.PlaceId) then
-	ScriptLoad = "HSz_ADS"
+	ScriptLoadHSz = "HSz_ADS"
 elseif table.find(PlaceId_ALS,game.PlaceId) then
-	ScriptLoad = "HSz_ALS"
+	ScriptLoadHSz = "HSz_ALS"
 end
 local hsz,err = pcall(function ()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/siradaniy/Script_HSz/main/" .. ScriptLoad .. ".lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/siradaniy/Script_HSz/main/" .. ScriptLoadHSz .. ".lua"))()
 end)
 
 if hsz == false then
