94cde52f2172836b144c190d0056386f6220d7e1
siradaniy <83637146+siradaniy@users.noreply.github.com>
2024-02-22 11:55:49 +0700

    Update HSz_Main.lua

--- HSz_Main.lua
+++ HSz_Main.lua
@@ -4,6 +4,7 @@ local PlaceId_AA = {8304191830,8349889591,10098525303,14229762361,14229839966,14
 local PlaceId_ADS = {6938803436,7274690025,7338881230,6990133340,6990129309,6990131029}
 local PlaceId_ALS = {12900046592,14368918515,12886143095}
 local PlaceId_ADF = {12135640403,15502802192,15502802094}
+local PlaceId_SB3 = {14819493140,14819490378,15695929915,11523257493,16468939104}
 
 if table.find(PlaceId_ACS,game.PlaceId) then
     ScriptLoadHSz = "HSz_ACS"
@@ -15,6 +16,8 @@ elseif table.find(PlaceId_ALS,game.PlaceId) then
 	ScriptLoadHSz = "HSz_ALS"
 elseif table.find(PlaceId_ADF,game.PlaceId) then
 	ScriptLoadHSz = "HSz_ADF"
+elseif table.find(PlaceId_SB3,game.PlaceId) then
+	ScriptLoadHSz = "HSz_SB3"
 end
 
 local hsz,err = pcall(function ()
